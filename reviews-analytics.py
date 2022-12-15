9bac013544132d6fe275e3f1fc34a0f57352fc52
dainalkai <120482248+dainalkai@users.noreply.github.com>
2022-12-15 14:58:16 +0800

    Add files via upload

--- reviews-analytics.py
+++ reviews-analytics.py
@@ -3,9 +3,12 @@ count = 0
 with open('053 reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
-		count += 1
+		total_len = len(line)
+		count += total_len
 		if count % 1000 == 0:
 			print(len(data))
-print(data[0])
-print('________________')
-print(data[1])		
+average_len = count/1000000			
+print('資料讀取完了，總共有', len(data), '筆資料')
+print('留言的平均長度為,', average_len)			
+
+	
